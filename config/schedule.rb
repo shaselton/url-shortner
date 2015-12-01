db9c8f45c15004d1d034d2a59eb9f4b3132a98ee
Scott Haselton <shaselton@gmail.com>
2015-12-01 15:20:19 -0800

    adding cron file

---
+++ config/schedule.rb
@@ -0,0 +1,17 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/var/www/shaselton.com/shared/log/cron_log.log"
+#
+
+
+# TODO: back up sqlite3?
+# every :day, :at => '12:20am' do
+#   command 'mysqldump --databases --password= fusion_invoice > /var/www/shaselton.com/db_backups/hase.io.sql'
+#   command "cd /var/www/shaselton.com/db_backups && git add invoice.shaselton.com.sql && git commit -m 'database backup' && git push origin master"
+# end
+
